bcc06950f1c23769afeeec4320c8cb3cd7a72589
Grafcheek <165086394+Grafcheek@users.noreply.github.com>
2025-05-30 16:26:18 +0000

    attaching the coursework code

--- Lab_1/sandbox/c_version.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Krylov\nGrigoriy\nVadimovich\n");
-    return 0;
-}
--- Lab_4/task1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int n;
-    scanf("%d", &n);
-    printf("%d\n", n / (37 * 13));
-    return 0;
-}
